28fa814ac9bc5b5ed1c194686ba396d8720c4388
Jim Barnett <james.barnett.jr@gmail.com>
2024-07-29 04:04:54 -0500

    Implemented validation, dialog, hover, and fixed shadow

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const button = document.querySelector(".content .button")
+const email = document.getElementById("email")
+const error = document.querySelector(".form-group span:last-child")
+const thanks = document.getElementsByTagName("dialog")[0]
+const content = document.querySelector(".content")
+const picture = document.getElementsByTagName("picture")[0]
 
-setupCounter(document.querySelector('#counter'))
+function validateMinimal(str) {
+  return str.length >= 5 && str.indexOf('@') > -1 && str.indexOf('.') > -1
+}
+
+button.addEventListener("click", (e) => {
+  if (!email.value || !email || !validateMinimal(email.value)) {
+    e.preventDefault();
+    error.style.display = "block"
+    return
+  } else {
+    const message = `A confirmation email has been sent to <span class="subscriber-email">${email.value}</span>.`
+    const p = thanks.querySelector("p")  
+    p.innerHTML = message
+    picture.style.display = "none"
+    content.style.display = "none"
+
+    thanks.style.display = "grid"
+    thanks.showModal()
+    e.preventDefault()
+  }
+});
+
+const closeButton = thanks.querySelector(".close")
+
+closeButton.addEventListener("click", (e) => {
+  thanks.close()
+  thanks.style.display = "none"
+  picture.style.display = "block"
+  content.style.display = "grid"
+  email.value = ""
+});
